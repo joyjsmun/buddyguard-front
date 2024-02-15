a9bda211e0d923c1261cb568f6b6650293a173d0
joyjsmun <joy.js.mun@gmail.com>
2024-02-15 16:32:35 +0900

    ADD: after onboarding page settings

---
+++ assets/images/index.js
@@ -0,0 +1,34 @@
+export { default as LogoImage } from "./logo.png";
+export { default as Map } from "./map_example.png";
+
+export { default as Avatar1 } from "./avatar1.png";
+export { default as Avatar2 } from "./avatar2.png";
+
+export { default as Hangout1 } from "./hangout1.png";
+export { default as Hangout2 } from "./hangout2.png";
+export { default as Hangout3 } from "./hangout3.png";
+export { default as Hangout4 } from "./hangout4.png";
+export { default as Hangout5 } from "./hangout5.png";
+
+export { default as ProfileImg } from "./profilePic.png";
+export { default as BarcodeImg } from "./barcode.png";
+
+export { default as poap1 } from "./poap1.png";
+export { default as poap2 } from "./poap2.png";
+export { default as poap3 } from "./poap3.png";
+export { default as poap4 } from "./poap4.png";
+
+export { default as nft1 } from "./nft1.png";
+export { default as nft2 } from "./nft2.png";
+export { default as nft3 } from "./nft3.png";
+export { default as nft4 } from "./nft4.png";
+
+export { default as image1 } from "./image1.png";
+export { default as image2 } from "./image2.png";
+export { default as image3 } from "./image3.png";
+
+export { default as zuzalu1 } from "./zuzalu1.png";
+export { default as vitalia } from "./vitalia.png";
+export { default as zuviliage } from "./zuviliage.png";
+export { default as devcon } from "./devcon.png";
+export { default as zuberlin } from "./zuberlin.png";
